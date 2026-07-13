d85524130fd6aad31f60c9cda6bcb5551835fcf0
ChaeHyunLim <chaehyun010104@gmail.com>
2026-07-13 19:40:09 +0900

    section 2 : JdbcTemplate 적용 2,3 - 구성과 실행

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -9,7 +9,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 
-@Import(MemoryConfig.class)
+// @Import(MemoryConfig.class)
+@Import(JdbcTemplateV1Config.class)
 // controller만 컴포넌트 스캔을 하겠다
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
